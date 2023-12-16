ef47b8fb85ebf211d94ddbba09f1a6fb55320236
EgorDyach <tvoysosed33@gmail.com>
2023-12-16 14:51:58 +0300

    added chat disign

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+        <div className="messageInfo">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Pierre-Person.jpg/1200px-Pierre-Person.jpg" alt="" />
+        <span>Just now</span>
+        </div>
+        <div className="messageContent">
+            <p>Hello</p>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Pierre-Person.jpg/1200px-Pierre-Person.jpg" alt="" />
+        </div>
+    </div>
   )
 }
 
